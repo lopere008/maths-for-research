715d44d107a1b3485db7d05ec99de3bd8c0f5e0f
lopere008 <fatchokossi@gmail.com>
2026-06-11 00:52:18 +0100

    Feat: combinaison lineaire

---
+++ 01_Linear_Algebra/linear_combinations.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+A = np.array([2,0.02])
+V = np.array([-3,0.04])
+new_t =  np.add(A,V)
+plt.figure(figsize=(6,6))
+plt.quiver(0,0, A[0], A[1], angles='xy', scale_units='xy', scale=1, color='blue',width=0.015, label='A')
+plt.quiver(0,0, V[0], V[1], angles='xy', scale_units='xy', scale=1, color='green',width=0.015, label='V')
+plt.quiver(0,0, new_t[0], new_t[1], angles='xy', scale_units='xy', scale=1, color='red',width=0.015, label='new_t')
+plt.xlim(-5, 5)
+plt.xlim(-5, 5)
+plt.axhline(0, color='black', linewidth=0.8)
+plt.axvline(0, color='black', linewidth=0.8)
+
+
+plt.grid(True, linestyle='--',alpha=0.6)
+plt.legend(loc='upper left')
+plt.title('vecteur résultant')
+plt.show()
+
+
